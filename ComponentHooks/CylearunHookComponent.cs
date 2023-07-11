08dc398db48da816bfb83e535acd2b118c1c90d7
hejiajun107 <hejiajun107@126.com>
2023-07-11 21:06:10 +0800

    Ammo同步子机数脚本

--- ComponentHooks/CylearunHookComponent.cs
+++ ComponentHooks/CylearunHookComponent.cs
@@ -8,7 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Extension.CY;
-
+using PatcherYRpp.FileFormats;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ComponentHooks
 {
@@ -63,5 +64,35 @@ namespace ComponentHooks
             }
             return 0;
         }
+
+
+        //[Hook(HookType.AresHook, Address = 0x73C5FC, Size = 6)]
+        //public static unsafe UInt32 UnitClass_DrawSHP(REGISTERS* R)
+        //{
+        //    Pointer<UnitClass> unit = (IntPtr)(void*)R->EBP;
+        //    var techno = unit.Convert<TechnoClass>();
+        //    TechnoExt ext = TechnoExt.ExtMap.Find(techno);
+        //    //techno.Ref.Base.
+        //    //Pointer<SHPStruct> Image = techno.Ref.
+
+
+        //    //if (UnitTypeClass * pCustomType = pData->GetUnitType())
+        //    //{
+        //    //    Image = pCustomType->GetImage();
+        //    //}
+
+        //    //if (Image)
+        //    //{
+        //    //    R->EAX<SHPStruct*>(Image);
+        //    //    return 0x73C602;
+        //    //}
+        //    //return 0x73CE00;
+
+        //    return 0;
+        //}
+
+
+     
+
     }
 }
---
+++ Projects/Scripts/SyncSpawnToAmmoScript.cs
@@ -0,0 +1,26 @@
+﻿using Extension.Ext;
+using Extension.Script;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    [Serializable]
+    public class SyncSpawnToAmmoScript : TechnoScriptable
+    {
+        public SyncSpawnToAmmoScript(TechnoExt owner) : base(owner)
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            if(Owner.OwnerObject.Ref.SpawnManager.IsNull)
+                return;
+
+            Owner.OwnerObject.Ref.Ammo = Owner.OwnerObject.Ref.SpawnManager.Ref.DrawState();
+        }
+    }
+}
