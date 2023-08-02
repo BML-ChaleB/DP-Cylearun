9737e7c4694b0d48081648435813aadc4b929878
hejiajun107 <hejiajun107@126.com>
2023-08-02 21:14:58 +0800

    击穿脚本

--- Projects/Extension.Cylearun/Common/BreakDown.cs
+++ Projects/Extension.Cylearun/Common/BreakDown.cs
@@ -1,6 +1,7 @@
 ﻿using Extension.CWUtilities;
 using Extension.INI;
 using PatcherYRpp;
+using PatcherYRpp.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,23 +19,94 @@ namespace Extension.CY
             if (Data.BreakDownResistance == -1)
                 return;
 
+            var x = Data.BreakDownResistance;
             WarheadINI.Section = pWH.Ref.Base.ID;
-            var whBreakDownLevel = WarheadINI.Data.BreakDownLevel;
+            var y = WarheadINI.Data.BreakDownLevel;
+            var p = WarheadINI.Data.BreakDownChanceReduce;
+            var t = WarheadINI.Data.OverBreakDownChanceReduce;
             var damageMultipler1 = WarheadINI.Data.NotBreakDown;
             var damageMultipler2 = WarheadINI.Data.BreakDown;
             var damageMultipler3 = WarheadINI.Data.OverBreakDown;
 
-            if (whBreakDownLevel == -1)
+            if (y == -1)
                 return;
 
-            if (Data.BreakDownResistance > 2 * whBreakDownLevel)
+            //无法击穿
+            if (x > 2 * y)
             {
-                pDamage.Ref = (int)(pDamage.Ref * damageMultipler1);
+                pDamage.Ref = (int)(pDamage.Ref * GetBreakDownMultipler(damageMultipler1));
+                var pAnim = AnimTypeClass.ABSTRACTTYPE_ARRAY.Find(DpSettingINI.Data.CantBreakDownAnim);
+                if (pAnim.IsNotNull)
+                {
+                    YRMemory.Create<AnimClass>(pAnim, Owner.OwnerObject.Ref.Base.Base.GetCoords());
+                }
+                return;
             }
 
-            
+
+            //未击穿
+            if (x < 2 * y && x > y)
+            {
+                int chance = x - y - p;
+                if (chance <= 0) return;
+                var rd = MathEx.Random.Next(0, 100);
+                if (rd <= chance)
+                {
+                    pDamage.Ref = (int)(pDamage.Ref * GetBreakDownMultipler(damageMultipler1));
+                    var pAnim = AnimTypeClass.ABSTRACTTYPE_ARRAY.Find(DpSettingINI.Data.NotBreakDownAnim);
+                    if (pAnim.IsNotNull)
+                    {
+                        YRMemory.Create<AnimClass>(pAnim, Owner.OwnerObject.Ref.Base.Base.GetCoords());
+                    }
+                }
+                return;
+            }
+
+
+            //击穿
+            if (y < 2 * x && y > x)
+            {
+                int chance = y - x - p;
+                if (chance <= 0) return;
+                var rd = MathEx.Random.Next(0, 100);
+                if (rd <= chance)
+                {
+                    pDamage.Ref = (int)(pDamage.Ref * GetBreakDownMultipler(damageMultipler2));
+                    var pAnim = AnimTypeClass.ABSTRACTTYPE_ARRAY.Find(DpSettingINI.Data.BreakDownAnim);
+                    if (pAnim.IsNotNull)
+                    {
+                        YRMemory.Create<AnimClass>(pAnim, Owner.OwnerObject.Ref.Base.Base.GetCoords());
+                    }
+                }
+                return;
+            }
+
+            //过度击穿
+            if (y > 2 * x)
+            {
+                int chance = y - x - t;
+                if (chance <= 0) return;
+                var rd = MathEx.Random.Next(0, 100);
+                if (rd <= chance)
+                {
+                    pDamage.Ref = (int)(pDamage.Ref * GetBreakDownMultipler(damageMultipler3));
+                    var pAnim = AnimTypeClass.ABSTRACTTYPE_ARRAY.Find(DpSettingINI.Data.OverBreakDownAnim);
+                    if (pAnim.IsNotNull)
+                    {
+                        YRMemory.Create<AnimClass>(pAnim, Owner.OwnerObject.Ref.Base.Base.GetCoords());
+                    }
+                }
+                return;
+            }
+
         }
 
+
+        private double GetBreakDownMultipler(double multilpler)
+        {
+            var intmult = (int)(multilpler * 100);
+            return MathEx.Random.Next(Math.Min(intmult, 100), Math.Max(intmult, 100)) / 100d;
+        }
     }
 
     public partial class TechnoGlobalTypeExt
@@ -58,5 +130,24 @@ namespace Extension.CY
         public double BreakDown = 1.0;
         [INIField(Key = "DamageMultipler.OverBreakDown")]
         public double OverBreakDown = 1.0;
+        [INIField(Key = "BreakDown.BreakDownChanceReduce")]
+        public int BreakDownChanceReduce = 0;
+        [INIField(Key = "BreakDown.OverBreakDownChanceReduce")]
+        public int OverBreakDownChanceReduce = 0;
+    }
+
+    public partial class GlobalSetting 
+    {
+        [INIField(Key = "BreakDown.CannotBreanDownAnim")]
+        public string CantBreakDownAnim;
+        [INIField(Key = "BreakDown.NotBreakDownAnim")]
+        public string NotBreakDownAnim;
+        [INIField(Key = "BreakDown.BreakDownAnim")]
+        public string BreakDownAnim;
+        [INIField(Key = "BreakDown.OverBreakDownAnim")]
+        public string OverBreakDownAnim;
+
+
+
     }
 }
