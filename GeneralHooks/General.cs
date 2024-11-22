70cb2d11f681d2ebae07b493251b1d2bec57d274
LoneSailSoon <155627210+LoneSailSoon@users.noreply.github.com>
2024-11-22 22:19:25 +0800

    Box Selection FillTrans

--- GeneralHooks/General.cs
+++ GeneralHooks/General.cs
@@ -1,4 +1,5 @@
 ﻿using DynamicPatcher;
+using PatcherYRpp;
 using PatcherYRpp.Utilities;
 using System;
 using System.Collections.Generic;
@@ -80,6 +81,29 @@ namespace GeneralHooks
             return 0;
         }
 
+        static readonly ColorStruct colorFill = new(0, 60, 176);
+        static readonly ColorStruct colorOutLine = new(64, 92, 240);
+        static readonly int trans = 30;
 
+        [Hook(HookType.AresHook, Address = 0x6DA217, Size = 7)]
+        public static unsafe UInt32 Box_Selection_FillTransb(REGISTERS* R)
+        {
+            static void fillTrans(Pointer<Surface> pSurface, RectangleStruct rect, ColorStruct color, int trans)
+            {
+                var func = (delegate* unmanaged[Thiscall]<ref Surface, ref RectangleStruct, ref ColorStruct, int, Bool>)((Pointer<Pointer<IntPtr>>)(IntPtr)pSurface)[0][7];
+                func(ref pSurface.Ref, ref rect, ref color, trans);
+            }
+
+            Pointer<RectangleStruct> pRect = R->lea_Stack<IntPtr>(28 + -16);
+
+            fillTrans(Surface.Current, pRect.Data, colorFill, trans);
+
+            Surface.Current.Ref.DrawRectEx(Surface.ViewBound, pRect.Ref, Drawing.Color16bit(colorOutLine));
+            pRect.Ref.Width = 0;
+            pRect.Ref.Height = 0;
+            pRect.Ref.X = 0;
+            pRect.Ref.Y = 0;
+            return 0;
+        }
     }
 }
