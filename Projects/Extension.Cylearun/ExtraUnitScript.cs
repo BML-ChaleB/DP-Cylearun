3d42af3a2200ddd45c3f2b1ddb75fa99e2e372f5
hejiajun107 <hejiajun107@126.com>
2026-05-27 21:24:20 +0800

    修复替身

--- Projects/Extension.Cylearun/ExtraUnitScript.cs
+++ Projects/Extension.Cylearun/ExtraUnitScript.cs
@@ -177,6 +177,7 @@ namespace Scripts
             {
                 if (!salve.IsNullOrExpired())
                 {
+                    salve.OwnerObject.Ref.Stun();
                     salve.OwnerObject.Ref.Base.Remove();
                     salve.OwnerObject.Ref.Base.RegisterKill(Owner.OwnerObject.Ref.Owner);
                     salve.OwnerObject.Ref.Base.UnInit();
